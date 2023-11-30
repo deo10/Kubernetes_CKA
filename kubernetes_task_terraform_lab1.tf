981683b723ff9e83b170300f1d3c1843c5a55f51
deo10 <pipazoglov@gmail.com>
2023-11-30 16:04:03 +0300

    minor changes

--- kubernetes_task_terraform_lab1.tf
+++ kubernetes_task_terraform_lab1.tf
@@ -6,10 +6,10 @@
 
 #!/bin/bash
 
-curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
-echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
-sudo apt update
-sudo apt install terraform=1.1.5
+#curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
+#echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+#sudo apt update
+#sudo apt install terraform=1.1.5
 
 
 #configure provider.tf for kubernetes
